b6c5337bb9c7dc2dc27d062c993573c158fbea91
Farratto <181971243+Farratto@users.noreply.github.com>
2024-12-09 22:33:31 -0500

    height support. bug fixes. hotkey support

--- scripts/manager_movement.lua
+++ scripts/manager_movement.lua
@@ -2,7 +2,7 @@
 -- attribution and copyright information.
 
 -- luacheck: globals getTokenPosition calcDistance updateDistTraveled processTurnStart getDistTraveled setStartPosi
--- luacheck: globals updateSpeedWindows
+-- luacheck: globals updateSpeedWindows updateOptionChange handleNewMap onHotKeyTravelDistance processTravelDist
 
 local tStartPosis = {};
 
@@ -10,27 +10,64 @@ function onInit()
 	if Session.IsHost then
 		if Session.RulesetName == "5E" then
 			OptionsManager.registerOptionData({	sKey = 'move_on', sGroupRes = 'option_header_WtW', tCustom = { default = "on" } });
-			OptionsManager.registerCallback('move_on', updateSpeedWindows);
-			CombatManager.setCustomTurnStart(processTurnStart);
+			OptionsManager.registerCallback('move_on', updateOptionChange);
+			if OptionsManager.isOption('move_on', 'on') then
+				CombatManager.setCustomTurnStart(processTurnStart);
+				DB.addHandler('combattracker.list.*.tokenrefid', 'onUpdate', handleNewMap);
+			end
 		end
 	end
+	Interface.addKeyedEventHandler("onHotkeyActivated", "traveleddistance", MovementManager.onHotKeyTravelDistance);
 end
 function onClose()
 	if Session.IsHost then
 		if Session.RulesetName == "5E" then
 			OptionsManager.unregisterCallback('move_on', updateSpeedWindows);
+			if OptionsManager.isOption('move_on', 'on') then
+				DB.removeHandler('combattracker.list.*.tokenrefid', 'onUpdate', handleNewMap);
+			end
 		end
 	end
+end
+
+function updateOptionChange()
+	if OptionsManager.isOption('move_on', 'on') then
+		CombatManager.setCustomTurnStart(processTurnStart);
+		DB.addHandler('combattracker.list.*.tokenrefid', 'onUpdate', handleNewMap);
+	else
+		DB.removeHandler('combattracker.list.*.tokenrefid', 'onUpdate', handleNewMap);
+	end
+	updateSpeedWindows();
 end
 
 function updateSpeedWindows()
 	local tSpeedWindows = Interface.getWindows('speed_window');
 	for _,v in ipairs(tSpeedWindows) do
-		v.speedwindowcontent.headertraveled.update();
-		v.speedwindowcontent.traveled.update();
-		v.speedwindowcontent.sub_buttons.reset.update();
-		v.speedwindowcontent.sub_buttons.check.update();
+		v.speedwindowcontent.subwindow.headertraveled.update();
+		v.speedwindowcontent.subwindow.traveled.update();
+		v.sub_buttons.subwindow.reset.update();
+		v.sub_buttons.subwindow.check.update();
 	end
+end
+
+function handleNewMap(tokenrefnode)
+	local nodeCT = DB.getParent(tokenrefnode);
+	setStartPosi(nodeCT);
+end
+
+function processTravelDist(nodeCT)
+	local nDist,sSuffix = getDistTraveled(nodeCT);
+	if nDist then
+		updateDistTraveled(nodeCT, nDist, true, sSuffix);
+	else
+		Debug.console("WalkThisWay.speed.xml - button_check_dist - not nDist");
+	end
+	setStartPosi(nodeCT);
+end
+
+function onHotKeyTravelDistance(draginfo)
+	local nodeCT = draginfo.getDatabaseNode();
+	processTravelDist(nodeCT);
 end
 
 function getTokenPosition(nodeCT)
@@ -38,28 +75,29 @@ function getTokenPosition(nodeCT)
 		Debug.console("MovementManager.getTokenPosition - not nodeCT");
 	end
 	local tokenCT = CombatManager.getTokenFromCT(nodeCT);
-	if not tokenCT then
-		Debug.console("MovementManager.getTokenPosition - not tokenCT");
-	else
-		return tokenCT.getPosition();
-	end
+	if tokenCT then return tokenCT.getPosition() end
 end
 
-function calcDistance(xStart, yStart, xCurrent, yCurrent)
+function calcDistance(xStart, yStart, xCurrent, yCurrent, hStart, hCurrent)
 	if not xStart or not yStart or not xCurrent or not yCurrent then
 		Debug.console("MovementManager.calcDistance - 18 - not xStart or not yStart or not xCurrent or not yCurrent");
 		return;
 	end
+	if not hStart then hStart = 0 end
+	if not hCurrent then hCurrent = 0 end
 	xStart = tonumber(xStart);
 	yStart = tonumber(yStart);
+	hStart = tonumber(hStart);
 	xCurrent = tonumber(xCurrent);
 	yCurrent = tonumber(yCurrent);
-	if not xStart or not yStart or not xCurrent or not yCurrent then
-		Debug.console("MovementManager.calcDistance - 27 - not xStart or not yStart or not xCurrent or not yCurrent");
+	hCurrent = tonumber(hCurrent);
+	if not xStart or not yStart or not xCurrent or not yCurrent or not hCurrent or not hStart then
+		Debug.console("MovementManager.calcDistance - not Start or not Current");
 		return;
 	end
 	local xDist = math.abs(xCurrent - xStart);
 	local yDist = math.abs(yCurrent - yStart);
+	local hDist = math.abs(hCurrent - hStart);
 
 	local sPref = OptionsManager.getOption('HRDD');
 	local nDiagMult;
@@ -78,50 +116,80 @@ function calcDistance(xStart, yStart, xCurrent, yCurrent)
 	local nLinDist;
 	if nDiagDistX > nDiagDistY then
 		nDiagDist = nDiagDistX;
-		nLinDist = nDiagDistX - nDiagDistRemainder;
+		nLinDist = nDiagDistRemainder - nDiagDistX;
 	elseif nDiagDistX < nDiagDistY then
 		nDiagDist = nDiagDistY;
-		nLinDist = nDiagDistY - nDiagDistRemainder;
+		nLinDist = nDiagDistRemainder - nDiagDistY;
 	else
 		nDiagDist = nDiagDistX;
 		nLinDist = 0;
 	end
 	local nDiagDistMult = nDiagDist * nDiagMult;
 	local nDistTotal = nDiagDistMult + nLinDist;
-	local nDistTotalRound = nDistTotal * 2;
+
+	local nDiagDistRemainderH = nDistTotal - hDist;
+	local nDiagDistH;
+	if nDiagDistRemainderH >= 0 then
+		nDiagDistH = hDist;
+	else
+		nDiagDistH = nDistTotal;
+		nDiagDistRemainderH = math.abs(nDiagDistRemainderH);
+	end
+	local nDiagDistMultH = nDiagDistH * nDiagMult;
+	local nDistTotalH = nDiagDistMultH + nDiagDistRemainderH;
+
+	local nDistTotalRound = nDistTotalH / 5;
 	nDistTotalRound = math.ceil(nDistTotalRound);
 	nDistTotalRound = nDistTotalRound / 2;
+
 	return nDistTotalRound;
 end
 
-function updateDistTraveled(nodeCT, nDist, bAdd)
+function updateDistTraveled(nodeCT, nDist, bAdd, sSuffix)
 	if nDist then nDist = tonumber(nDist) end
 	if not nodeCT or not nDist then
-		Debug.console("MovementManager.updateDistCovered - not nodeCT or not nDist");
+		Debug.console("MovementManager.updateDistTraveled - not nodeCT or not nDist");
+		return;
 	end
 	local nodeCTWtW = DB.createChild(nodeCT, 'WalkThisWay');
 	local nodeTraveled = DB.createChild(nodeCTWtW, 'traveled', 'string');
 	local sTravelCurrent = DB.getValue(nodeTraveled);
 	local nTravelCurrent;
-	if sTravelCurrent then tonumber(nTravelCurrent) end
+	if sTravelCurrent then
+		nTravelCurrent = string.match(sTravelCurrent, '^%d+');
+		nTravelCurrent = tonumber(nTravelCurrent)
+	end
 	if not nTravelCurrent or not bAdd then
 		nTravelCurrent = 0;
 	end
 	local nTraveled = nTravelCurrent + nDist;
-	local sTraveled = tostring(nTraveled);
+	if not sSuffix then
+		sSuffix = '';
+	else
+		if sSuffix == "'" then sSuffix = "ft." end
+	end
+	local sTraveled = tostring(nTraveled).." "..sSuffix;
 	DB.setValue(nodeCTWtW, 'traveled', 'string', sTraveled);
 end
 
 function processTurnStart(_)
+	if OptionsManager.isOption('move_on', 'off') then
+		return;
+	end
 	tStartPosis = {};
 	for _,nodeCT in ipairs(CombatManager.getAllCombatantNodes()) do
-		local tPosi = {};
-		local xStart, yStart = getTokenPosition(nodeCT);
-		tPosi['x'] = xStart;
-		tPosi['y'] = yStart;
-		tPosi['nodeCT'] = nodeCT;
-		table.insert(tStartPosis, tPosi);
-		updateDistTraveled(nodeCT, 0);
+		local tokenCT = CombatManager.getTokenFromCT(nodeCT);
+		if tokenCT then
+			local tPosi = {};
+			local xStart, yStart = getTokenPosition(nodeCT);
+			local hStart = tokenCT.getHeight();
+			tPosi['x'] = xStart;
+			tPosi['y'] = yStart;
+			tPosi['h'] = hStart;
+			tPosi['nodeCT'] = nodeCT;
+			table.insert(tStartPosis, tPosi);
+			updateDistTraveled(nodeCT, 0);
+		end
 	end
 end
 
@@ -129,17 +197,29 @@ function getDistTraveled(nodeCT)
 	if not nodeCT then
 		Debug.console("MovementManager.getDistTraveled - not nodeCT");
 	end
+	local tokenCT = CombatManager.getTokenFromCT(nodeCT);
+	if not tokenCT then
+		Debug.console("MovementManager.getDistTraveled - not nodeCT");
+		return;
+	end
 	local xCurrent, yCurrent = getTokenPosition(nodeCT);
+	local hCurrent = tokenCT.getHeight();
 	local xStart;
 	local yStart;
+	local hStart;
 	for _,v in ipairs(tStartPosis) do
 		if v.nodeCT == nodeCT then
 			xStart = v.x;
 			yStart = v.y;
+			hStart = v.h;
 		end
 	end
-	local nDist = calcDistance(xStart, yStart, xCurrent, yCurrent);
-	return nDist;
+	local nodeContainer = tokenCT.getContainerNode();
+	local nImageDistUnits = Image.getDistanceBaseUnits(nodeContainer);
+	local sImageDistSuffix = Image.getDistanceSuffix(nodeContainer);
+	local nDist = calcDistance(xStart, yStart, xCurrent, yCurrent, hStart, hCurrent);
+	local nDistAdj = nDist * nImageDistUnits;
+	return nDistAdj, sImageDistSuffix;
 end
 
 function setStartPosi(nodeCT)
@@ -150,17 +230,27 @@ function setStartPosi(nodeCT)
 	for _,v in ipairs(tStartPosis) do
 		if v.nodeCT == nodeCT then
 			bFound = true;
-			local xStart, yStart = getTokenPosition(nodeCT);
-			v.x = xStart;
-			v.y = yStart;
+			local tokenCT = CombatManager.getTokenFromCT(nodeCT);
+			if tokenCT then
+				local xStart, yStart = getTokenPosition(nodeCT);
+				local hStart = tokenCT.getHeight();
+				v.x = xStart;
+				v.y = yStart;
+				v.h = hStart;
+			end
 		end
 	end
 	if not bFound then
-		local tPosi = {};
-		local xStart, yStart = getTokenPosition(nodeCT);
-		tPosi['x'] = xStart;
-		tPosi['y'] = yStart;
-		tPosi['nodeCT'] = nodeCT;
-		table.insert(tStartPosis, tPosi);
+		local tokenCT = CombatManager.getTokenFromCT(nodeCT);
+		if tokenCT then
+			local tPosi = {};
+			local xStart, yStart = getTokenPosition(nodeCT);
+			local hStart = tokenCT.getHeight();
+			tPosi['x'] = xStart;
+			tPosi['y'] = yStart;
+			tPosi['h'] = hStart;
+			tPosi['nodeCT'] = nodeCT;
+			table.insert(tStartPosis, tPosi);
+		end
 	end
 end
